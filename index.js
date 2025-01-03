c8f0bf676904bcc3dfa2e69e89a20d07665a2d8d
[NilaBeast] <[manika.basak1977@gmail.com]>
2025-01-03 12:33:42 +0530

    Express notes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app =express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 //Middle ware explained
 app.use((req, res, next) => {
     console.log("Middleware used and request forwaded");
